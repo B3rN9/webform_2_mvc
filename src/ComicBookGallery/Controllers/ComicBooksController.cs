c729dab3b365f016d2a53d01370c3b4f7864ea91
Berna <Berna@ZERO>
2016-07-06 11:21:45 -0400

    Updated comic book detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,12 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness as <strong>Peter Parkes</strong> leaves this world :(</p>";
+            ViewBag.Artists = new string[] { "Script: Dan Slott", "Pencils: Humberto Ramos", "Inks: Victor Olazaba", "Colors: Edgar Delgado", "Letters: Chris Eliopoulus" };
+
             return View();
         }
     }
