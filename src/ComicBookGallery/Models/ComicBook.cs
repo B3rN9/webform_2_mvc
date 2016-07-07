9430c47b19eceafc57efe3d20eca436339a4eebe
Berna <Berna@ZERO>
2016-07-07 10:59:40 -0400

    Updated comic book model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -13,5 +13,21 @@ namespace ComicBookGallery.Models
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; } 
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        public string DisplayImg
+        {
+            get
+            {
+                return SeriesTitle.ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
