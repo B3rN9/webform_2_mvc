c38b6fbcb22758a65a9b95ae7912ce3416eed38c
Berna <Berna@ZERO>
2016-07-12 10:32:30 -0400

    Innit commit

---
+++ mvc_2_webform_mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc_2_webform_mvc.Startup))]
+namespace mvc_2_webform_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
